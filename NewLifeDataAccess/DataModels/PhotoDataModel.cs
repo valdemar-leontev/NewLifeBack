aad25a9f4ee4abe7d8b2aecc19cd50e1ffc44d56
valdemar-leontev <valdemar.leontev@gmail.com>
2024-06-23 22:20:27 +0300

    Fix photoDataModel

--- NewLifeDataAccess/DataModels/PhotoDataModel.cs
+++ NewLifeDataAccess/DataModels/PhotoDataModel.cs
@@ -4,9 +4,9 @@ public class PhotoDataModel
 {
   public int Id { get; set; }
 
-  public string Title { get; set; }
+  public required string Title { get; set; }
 
-  public string FilePath { get; set; }
+  public required string FilePath { get; set; }
 
   public DateTime UploadedAt { get; set; }
 }
